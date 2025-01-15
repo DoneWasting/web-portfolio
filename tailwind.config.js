e4e4b608428fcf9ed6a7b9a00a78dbb9f34ad02d
David <davidsequeralozada@gmail.com>
2025-01-15 12:37:44 -0400

    Adjustes colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,15 +2,18 @@
 module.exports = {
   content: ["./docs/**/*.{html,js}"],
   theme: {
-    extend: {   colors: {
-      main:"#222831",
-      secondary: "#2A3335",
-      mainblue: "#00ADB5",
-      mainwhite:"#EEEEEE",
-      textGreyish: "#F8F8F2CC",
-      test: "#1D1B22"
-
-    },},
+    extend: {   
+      colors: {
+        main:"#222831",
+        secondary: "#393E46",
+        secondary2: "#2A3335",
+        mainYellow: "#fca311",
+        mainblue: "#00ADB5",
+        mainwhite:"#EEEEEE",
+        textGreyish: "#F8F8F2CC",
+        test: "#1D1B22"
   },
+  },
+},
   plugins: [],
 }
